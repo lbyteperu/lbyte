8290c1d7b703a61dbeac2dc3be07509a91e20f84
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2023-09-03 07:39:39 -0500

    reset

--- src/utils/AppConfig.ts
+++
@@ -1,8 +0,0 @@
-export const AppConfig = {
-  site_name: 'Lbyte.pe',
-  title: 'Lbyte Perú',
-  description: 'Software Company in Perú',
-  author: 'Ruben Paz Chuspe',
-  locale_region: 'es-pe',
-  locale: 'es',
-};
